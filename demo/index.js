5bacf13a6477bc0de31b8b7692ce3bfeb306afc4
PaulHan <zhangyu.han@outlook.com>
2017-11-14 18:05:25 +0800

    [UPDATED] demo重构

--- demo/index.js
+++ demo/index.js
@@ -3,9 +3,21 @@
  * @author PaulHan
  * @date 2017/11/13
  */
-import React, {PropTypes} from 'react';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
-class Test extends React.Component {
+console.log('React.Component', Component);
+const reactComponent = new Component();
+console.log('reactComponent', reactComponent);
+
+// 一个基本的测试
+class Test extends Component {
+  constructor(props) {
+    super();
+    this.state = {text: props.msg}
+  }
+
   render() {
     const {msg} = this.props;
     return (
@@ -18,4 +30,15 @@ Test.propTypes = {
   msg: PropTypes.string,
 };
 
-export default React.renderDOM(<Test msg="hello word"/>, document.getElementById('root'));
+const testComponent = new Test({msg: 'test'});
+console.log('testComponent', testComponent);
+
+class App extends React.Component {
+  render() {
+    const node = <Test msg="hello word"/>;
+    console.log(node);
+    return <div>{node}</div>;
+  }
+}
+
+export default ReactDOM.render(<App/>, document.getElementById('root'));
--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -3,15 +3,16 @@
  * @author PaulHan
  * @date 2017/11/13
  */
-var webpack = require('webpack');
+const webpack = require('webpack');
 const path = require('path');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './demo/index.js',
   output: {
-    path: path.resolve(__dirname, "dist/"),
-    filename: "bundle.js"
+    path: path.resolve(__dirname, 'dist/'),
+    filename: 'bundle.js'
   },
   module: {
     rules: [{
@@ -19,7 +20,7 @@ module.exports = {
       exclude: /node_modules/,
       loader: 'babel-loader',
       options: {
-        presets: ["es2015"]
+        presets: ['es2015']
       }
     }, {
       test: /\.css$/,
@@ -30,22 +31,38 @@ module.exports = {
     }, {
       test: /\.(png|jpg)$/,
       loader: 'url-loader?limit=512'
+    }, {
+      test: /\.js$/, // react,fbjs使用rollup替换，这里使用webpack方案
+      include: [path.resolve(__dirname, '../packages/')],
+      loader: 'string-replace-loader',
+      query: {
+        search: '__DEV__',
+        replace: 'true',
+        flags: 'g'
+      }
     }]
   },
   devtool: "cheap-module-source-map",
   plugins: [
-    new ExtractTextPlugin('bundle.css', {allChunks: true}),
+    // new ExtractTextPlugin('bundle.css', {allChunks: true}),
+    // new webpack.DefinePlugin({ // 未编译的react不会使用它
+    //   'process.env.NODE_ENV': '"development"',
+    // }),
+    new HtmlWebpackPlugin({
+      title: 'Hello App',
+      template: path.resolve(__dirname, 'index.html')
+    }),
   ],
   resolve: {
     modules: [
-      path.resolve(__dirname, "../packages"),
-      "node_modules", // 在启用了alias之后，必须设置，不然找不到
+      path.resolve(__dirname, '../packages'),
+      'node_modules', // 在启用了alias之后，必须设置，不然找不到
     ],
     alias: {
       "react": 'react',
       'react-dom': 'react-dom',
       'shared': 'shared',
-      'fbjs': 'fbjs',
+      'fbjs': 'fbjs', // 重定向fbjs的位置
     },
   },
 };
